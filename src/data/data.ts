a847a8083cb31b2d757c1c88ab297cfda3f43537
BrentTB <btbutkow@gmail.com>
2026-01-25 13:00:42 +0200

    add more achievements, and format achievements on mobile

--- src/data/data.ts
+++ src/data/data.ts
@@ -143,13 +143,13 @@ export const achievements: Achievement[] = [
     year: 2024,
     title: 'Graduated with Distinction',
     description:
-      'Bachelor of Science in Information Engineering from University of the Witwatersrand',
+      'Bachelor of Science in Information Engineering from the University of the Witwatersrand',
     link: 'https://www.wits.ac.za/',
   },
   {
     year: 2024,
     title: 'Top 5 in Information Engineering',
-    description: 'Ranked among the top 5 students in the Information Engineering program',
+    description: 'Ranked among the top 5 students in the Information Engineering degree',
   },
   {
     year: 2024,
@@ -167,14 +167,56 @@ export const achievements: Achievement[] = [
     description: 'Top Software Development III student',
   },
   {
-    year: 2022,
-    title: 'Launched Product Studio',
+    year: 2024,
+    title: 'Entelect University Challenge',
+    description: '4th place in the Entelect University Challenge',
+    link: 'https://challenge.entelect.co.za/university',
+  },
+  {
+    year: 2024,
+    title: 'Syft Hackathon',
+    description: 'Top 30 in the Syft Analytics Hackathon',
+  },
+  {
+    year: 2023,
+    title: 'Entelect University Challenge',
+    description: '2nd place in the Entelect University Challenge',
+    link: 'https://challenge.entelect.co.za/university',
+  },
+  {
+    year: 2023,
+    title: 'Isazi Award',
+    description: 'Awarded to the top 5 Information Engineers',
+  },
+  {
+    year: 2021,
+    title: 'International Olympiad in Informatics (IOI) Qualifier',
+    description: 'Selected as 1 of 4 students to represent South Africa at the IOI in Singapore',
+    link: 'https://ioi2021.sg/',
+  },
+  {
+    year: 2021,
+    title: 'Wits Maths Competition',
     description:
-      'Founded an agency to build and launch client projects across fintech, analytics, and SaaS',
+      '3rd place nationally in the Wits Maths Competition as a part of "Divide and Conquer"',
+    link: 'https://wmc.ms.wits.ac.za/2021-winners',
   },
   {
     year: 2020,
-    title: 'First Client Project',
-    description: 'Successfully delivered and launched first freelance web application project',
+    title: 'Programming Olympiad',
+    description: 'Top 3 nationally in the South African Programming Olympiad',
+    link: 'https://olympiad.org.za/past-winners/programming',
+  },
+  {
+    year: 2020,
+    title: 'Mathematical Olympiad',
+    description: 'Top 100 nationally in the South African Mathematical Olympiad',
+    link: 'https://www.samf.ac.za/en/sa-mathematics-olympiad',
+  },
+  {
+    year: 2020,
+    title: 'Wits Maths Competition',
+    description: '3rd in Gauteng in the Wits Maths Competition',
+    link: 'https://wmc.ms.wits.ac.za/2020-gauteng-winners',
   },
 ]
